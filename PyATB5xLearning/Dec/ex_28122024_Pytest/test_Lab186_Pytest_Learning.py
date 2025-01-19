091ed613dfa4f04c213ddeb350e396473c2fa19a
SwapnilGHb <swapnilbahe@gmail.com>
2025-01-19 13:48:16 +0530

    Allure reports files commit

---
+++ PyATB5xLearning/Dec/ex_28122024_Pytest/test_Lab186_Pytest_Learning.py
@@ -0,0 +1,6 @@
+def method1():
+    print("Hello World")
+
+def test_method2():
+    print("This is Pytest case")
+    assert True == False
---
+++ PyATB5xLearning/allure1/test_LearningAllure.py
@@ -0,0 +1,24 @@
+import pytest
+import allure
+
+@allure.title("Verify that create booking,with valid data is working")
+@allure.description("This testcase check for the positive create booking")
+@pytest.mark.positive
+def test_create_booking_positive():
+    print("test1")
+    assert 1-1 == 2
+
+@allure.title("Verify that create booking,with invalid data is working")
+@allure.description("This testcase check for the negative create booking")
+@pytest.mark.regression
+def test_create_booking_negative1():
+    print("test2")
+    assert 1+1 == 2
+
+
+@allure.title("Verify that create booking,with invalid data is working")
+@allure.description("This testcase check for the negative create booking")
+@pytest.mark.regression
+def test_create_booking_negative2():
+    print("test2")
+    assert 1+1 == 2
